ff3029cbc4922ce270150f9fab5ac01a2a6b553e
Hridayesh1605 <hridayeshmore16@gmail.com>
2024-05-21 19:07:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Card from './card'
+import sdata from "./data";
+
+
+//arrow funvtion
+const ncard = (val) => {
+  return (
+    <Card imgsrc={val.imgsrc}
+      title={val.title}
+      category={val.category}
+      link={val.link}
+    />
+
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="heading_style"> Top 5 Netflix Movies</h1>
+      {/* using props inside card
+
+      <Card imgsrc="https://i.ibb.co/CWChZsc/desktop-wallpaper-money-heist-netflix-web-series-thumbnail.jpg"
+        title="Money Heist"
+        category="netflix origina series"
+        link="https://www.netflix.com/in/title/80192098"
+      /> */}
+      {/* using array */}
+      {/* <Card imgsrc={sdata[0].imgsrc}
+        title={sdata[0].title}
+        category={sdata[0].category}
+        link={sdata[0].link}
+      /> */}
+      {/* {sdata.map(ncard)} */}
+
+
+      {/* //map function using arrow function// */}
+
+      {sdata.map((val) => {
+        return (
+          <Card
+            key={val.id}
+            imgsrc={val.imgsrc}
+            title={val.title}
+            category={val.category}
+            link={val.link}
+          />
+
+        )
+
+      })}
+    </>
+
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,32 @@
+const data = [
+    {
+        id:1,
+        imgsrc: "https://i.ibb.co/CWChZsc/desktop-wallpaper-money-heist-netflix-web-series-thumbnail.jpg",
+        title: "Money Heist",
+        category: "netflix origina series",
+        link: "https://www.netflix.com/in/title/80192098"
+    },
+    {
+        id:2,
+        imgsrc: "https://i.ibb.co/SBdtyz1/lucifer-netflix-web-series.jpg",
+        title: "lucifer", category: "netflix origina series", link: "https://www.netflix.com/in/title/80057918"
+    },
+    {
+        id:3,
+        imgsrc: "https://i.ibb.co/XJpMpxv/MV5-BMDZk-Ym-Vh-Nj-Mt-NWU4-MC00-MDQx-LWE3-Mj-Yt-ZGMz-ZWI1-Zjhl-OWJm-Xk-Ey-Xk-Fqc-Gde-QXVy-MTkx-Nj-Uy.jpg",
+        title: "Stranger things", category: "netflix origina series", link: "https://www.netflix.com/in/title/80057281"
+    },
+    {
+        id:4,
+        imgsrc: "https://i.ibb.co/MDmsBP4/MV5-BYzc4-Zm-Fh-Zj-It-OWJj-My00-Yzhi-LTkx-MDAt-NWUy-Nj-Iy-OTJk-Ym-Y3-Xk-Ey-Xk-Fqc-Gde-QXVy-NDAz-NDk0.jpg",
+        title: "she", category: "netflix origina series", link: "https://www.netflix.com/in/title/81183494"
+    },
+    {
+        id:5,
+        imgsrc: "https://i.ibb.co/frRzkyD/Wednesday-Netflix-series-poster.png",
+        title: "Wednesday", category: "netflix origina series", link: "https://www.netflix.com/in/title/81231974"
+    }
+
+]
+
+export default data;
